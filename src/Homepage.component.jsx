3d092584ed230442a49c0d59348ca647925e172e
Mina Wagieh <minawagieh@gmail.com>
2019-12-29 11:56:19 +0200

    add title in homepage

--- src/Homepage.component.jsx
+++ src/Homepage.component.jsx
@@ -2,52 +2,44 @@ import React from 'react';
 import './homepage.scss';
 
 const HomePage = () => {
-    return (  
-        // Main container
+    return (
+      // Main container
+      <div className="homepage">
+        <h1>Welcome to my Homepage</h1>
         <div className="directory-menu">
-            {/* Menu item */}
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">Hats</h1>
-                    <span className="subtitle">
-                        Shop Now
-                    </span>
-                </div>
+          {/* Menu item */}
+          <div className="menu-item">
+            <div className="content">
+              <h1 className="title">Hats</h1>
+              <span className="subtitle">Shop Now</span>
             </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">Jackets</h1>
-                    <span className="subtitle">
-                        Shop Now
-                    </span>
-                </div>
+          </div>
+          <div className="menu-item">
+            <div className="content">
+              <h1 className="title">Jackets</h1>
+              <span className="subtitle">Shop Now</span>
             </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">Sneackers</h1>
-                    <span className="subtitle">
-                        Shop Now
-                    </span>
-                </div>
+          </div>
+          <div className="menu-item">
+            <div className="content">
+              <h1 className="title">Sneackers</h1>
+              <span className="subtitle">Shop Now</span>
             </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">Men Shirts</h1>
-                    <span className="subtitle">
-                        Shop Now
-                    </span>
-                </div>
+          </div>
+          <div className="menu-item">
+            <div className="content">
+              <h1 className="title">Men Shirts</h1>
+              <span className="subtitle">Shop Now</span>
             </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">Women Shirts</h1>
-                    <span className="subtitle">
-                        Shop Now
-                    </span>
-                </div>
+          </div>
+          <div className="menu-item">
+            <div className="content">
+              <h1 className="title">Women Shirts</h1>
+              <span className="subtitle">Shop Now</span>
             </div>
-            
+          </div>
         </div>
+      </div>
     );
 }
  
